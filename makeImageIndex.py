38c2c82a42ef8b08023f3aa245df34231809ae56
nikischwdrtr <40233850+nikischwdrtr@users.noreply.github.com>
2021-12-12 20:10:32 +0100

    Add files via upload

---
+++ makeImageIndex.py
@@ -0,0 +1,70 @@
+# tool which lists all images (jpg/png/gif) in a folder
+# it puts it in new text file 
+# perfectly formatted for javascript array objects
+
+import glob
+import os
+from PIL import Image
+
+# choose folder
+mydir = "./"
+
+# get list of files
+jpg_list = glob.glob(mydir + "*.jpg")
+png_list = glob.glob(mydir + "*.png")
+gif_list = glob.glob(mydir + "*.gif")
+
+# get list of resolution
+jpg_reso = []
+for x in range(len(jpg_list)):
+    img = Image.open(jpg_list[x])
+    width, height = img.size
+    jpg_reso.append(str(width)+'x'+str(height))
+png_reso = []
+for x in range(len(png_list)):
+    img = Image.open(png_list[x])
+    width, height = img.size
+    png_reso.append(str(width)+'x'+str(height))
+gif_reso = []
+for x in range(len(gif_list)):
+    img = Image.open(gif_list[x])
+    width, height = img.size
+    gif_reso.append(str(width)+'x'+str(height))
+
+# get list of size
+jpg_size = []
+for x in range(len(jpg_list)):
+    img = os.path.getsize(jpg_list[x])
+    jpg_size.append(str(round(img/1000))+'kb')
+png_size = []
+for x in range(len(png_list)):
+    img = os.path.getsize(png_list[x])
+    png_size.append(str(round(img/1000))+'kb')
+gif_size = []
+for x in range(len(gif_list)):
+    img = os.path.getsize(gif_list[x])
+    gif_size.append(str(round(img/1000))+'kb')
+
+# clean file name list
+jpg_list = [e[2:] for e in jpg_list]
+png_list = [e[2:] for e in png_list]
+gif_list = [e[2:] for e in gif_list]
+
+# join lists
+imageNames = jpg_list + png_list + gif_list
+imageSizes = jpg_size + png_size + gif_size
+imageResos = jpg_reso + png_reso + gif_reso
+
+# create txt file
+f = open("!ImageIndex.txt","w+")
+
+# write txt file
+for x in range(len(imageNames)):
+    f.write("{ \n")
+    f.write("   id: "+str(x+1)+", \n")
+    f.write("   src: 'images/archive/"+imageNames[x]+"', \n")
+    f.write("   title: '"+imageNames[x]+"', \n")
+    f.write("   size: '"+imageSizes[x]+"', \n")
+    f.write("   reso: '"+imageResos[x]+"', \n")
+    f.write("}, \n")
+f.close()
---
+++ rename.py
@@ -0,0 +1,9 @@
+# rename files from 1 - ...
+
+import os
+path = '/Users/niklausiff/Desktop/first'
+files = os.listdir(path)
+
+
+for index, file in enumerate(files):
+    os.rename(os.path.join(path, file), os.path.join(path, ''.join([str(index), '.jpg'])))
